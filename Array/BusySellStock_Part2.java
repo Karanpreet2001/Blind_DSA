08d468d660baa7c585f8f7a2f09cd2ecb79f2859
Karanpreet <karanpreetfarwaha@gmail.com>
2022-09-25 03:44:31 -0700

    Arrays

--- Array/BusySellStock_Part2.java
+++ Array/BusySellStock_Part2.java
@@ -32,7 +32,7 @@ public class BusySellStock_Part2 {
         int profit=0;
 
         for (int i = 1; i < prices.length; i++) {
-            if(prices[i]\ > prices[i-1]){
+            if(prices[i] > prices[i-1]){
                 profit += prices[i] - prices[i-1];
             }
         }
